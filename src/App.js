4652e171e73c7f1dc8e4e1fc58c814b56bb3df7b
Marsha Hall <marshahall@pursuit.org>
2021-05-19 22:06:14 -0400

    get api call (#3)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+
+import Videos from "./Components/Videos";
 import About from './Components/About';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
+			<Videos />
      <About />
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
