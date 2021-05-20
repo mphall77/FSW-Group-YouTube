131bfe19f65b068da2f6791bb487438b95f513ac
Coreen Cooper <coreencooper@pursuit.org>
2021-05-20 07:14:24 -0400

    add key to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import About from './Components/About';
+import Videos from './Components/Videos';
 
 function App() {
   return (
     <div className="App">
      <About />
+     <Videos />
     </div>
   );
 }
