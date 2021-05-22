11a08475927ba42438de1edec3e4ae9e3d8bc392
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-05-22 09:23:35 -0400

    adding form to nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import "./App.css";
 import About from "./Components/About";
+import NavBar from "./Components/NavBar";
+import Home from "./Components/Home";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
