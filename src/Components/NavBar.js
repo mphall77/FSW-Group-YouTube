2a3418f7cb7c478ba10e25216100aca6207fb1bc
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-05-23 14:16:28 -0400

    styling

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,7 @@ import "../Styles/NavBar.css";
 
 const NavBar = () => {
 	return (
-		<nav className="NavBarContainer">
+		<nav className="navbar-container">
 			<NavLink to={"/"}>
 				<img
 					src="https://lh3.googleusercontent.com/M1aDvpQLc4SdHf05mKC-0EvpkRaKGc9E2Tqn_g434YiIY_H3Q_8H3aFWZi_GnifdQn_1cQ=s50"
--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
 
 const Video = () => {
- const { id } = useParams();
+  const { id } = useParams();
 
   return (
-    
-    <section>
+    <section className="video-container">
       <iframe
+        className="single-video"
         src={`https://www.youtube.com/embed/${id}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
