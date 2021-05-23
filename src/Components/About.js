75da86dad08a7f76becdbf923ce361dfbc9589d6
Marsha Hall <marshahall@pursuit.org>
2021-05-23 18:01:32 -0400

    style about section (#17)

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,62 +1,68 @@
 import "../Styles/About.css";
+import Kenia from "../Images/KeniaR.jpg";
+import Coreen from "../Images/CoreenC.png";
+import Marsha from "../Images/MarshaH.jpg";
 
-const projectDescription = "React based video streaming application using the YouTube API to source content. Layout styled using a combination of Flexbox and Grid.";
+const projectDescription =
+	"React based video streaming application using the YouTube API to source content. Layout styled using a combination of Flexbox and Grid.";
 
 const teammates = [
-  {
-    name: "Kenia",
-    gitHub: "https://github.com/kenia-r",
-    bio: "Mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque",
-  },
+	{
+		name: "Kenia",
+		gitHub: "https://github.com/kenia-r",
+		bio: "Mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque",
+		image: Kenia,
+	},
 
-  {
-    name: "Marsha",
-    gitHub: "https://github.com/mphall77",
-    bio: "Ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis.",
-  },
+	{
+		name: "Marsha",
+		gitHub: "https://github.com/mphall77",
+		bio: "Ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis.",
+		image: Marsha,
+	},
 
-  {
-    name: "Coreen",
-    gitHub: "https://github.com/CoreenCooper",
-    bio: "Client-focused software developer who is community-minded and has a passion for developing applications to improve user accessibility.",
-  },
+	{
+		name: "Coreen",
+		gitHub: "https://github.com/CoreenCooper",
+		bio: "Client-focused software developer who is community-minded and has a passion for developing applications to improve user accessibility.",
+		image: Coreen,
+	},
 ];
 
 const About = () => {
-  return (
-    <section className="about-container">
-      <section id="ProductDescription">
-        <h2>Project Description</h2>
+	return (
+		<section className="about-container">
+			<section id="ProjectDescription">
+				<h2>Project Description</h2>
 
-        <p>{projectDescription}</p>
+				<p>{projectDescription}</p>
+			</section>
 
-      </section>
+			<hr />
+			<div className="break"></div>
 
-      <div className="break"></div>
+			<section>
+				<h2 id="teamTitle">Meet the Team 👋🏼 👋🏽 👋🏾</h2>
 
-      <section>
+				<section className="teammates">
+					<ul id="TeamDescriptions">
+						{teammates.map((teammate) => {
+							return (
+								<li key={teammate.name}>
+									<img src={teammate.image} alt="profile" className="profile" />
+									<a href={teammate.gitHub} target="_blank" rel="noreferrer">
+										<h3 className="teammate-name">{teammate.name}</h3>
+									</a>
 
-        <h2 id="teamTitle">Meet the Team 👋🏼 👋🏽 👋🏾</h2>
-
-        <section>
-          <ul className="teammates" id="TeamDescriptions">
-            {teammates.map((teammate) => {
-              return (
-
-                  <li key={teammate.name}>
-                    <a href={teammate.gitHub} target="_blank"rel="noreferrer">            
-                      <h3>{teammate.name}</h3>
-                    </a>
-                    <p>{teammate.bio}</p>
-                  </li>
-              );
-            })}
-          </ul>
-
-        </section>
-      </section>
-    </section>
-  );
+									<p>{teammate.bio}</p>
+								</li>
+							);
+						})}
+					</ul>
+				</section>
+			</section>
+		</section>
+	);
 };
 
 export default About;
